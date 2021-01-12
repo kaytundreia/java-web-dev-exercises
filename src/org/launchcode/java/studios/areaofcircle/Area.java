52705ea640d7a831bdcaf06e39cb4501f0be3a7e
kaytundreia <74748908+kaytundreia@users.noreply.github.com>
2021-01-11 19:54:01 -0600

    Commit area of circle

---
+++ src/org/launchcode/java/studios/areaofcircle/Area.java
@@ -0,0 +1,15 @@
+package org.launchcode.java.studios.areaofcircle;
+
+import java.util.Scanner;
+//import java.lang.Math.*;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        Double radius = input.nextDouble();
+        //Double area = Math.PI * radius * radius;
+        System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
+    }
+
+}
